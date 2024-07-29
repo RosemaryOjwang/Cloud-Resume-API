8e1bcae12ce577c0cacd4f3357ec42e54db68b54
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2024-07-29 23:49:37 +0300

    edit file name from variables.tf to variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "AWS_ACCESS_KEY_ID"   {
+    description = "AWS user access key"
+}
+
+variable "AWS_SECRET_KEY"   {
+    description = "AWS user access secret key"
+}
+
+variable "AWS_REGION" {
+    description = "AWS resouces region"
+}
