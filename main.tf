1e1c8b8832c60278340c8267967988464ce32b33
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2024-07-30 07:10:14 +0300

    modify

--- main.tf
+++ main.tf
@@ -7,11 +7,13 @@ terraform {
     }
   }
 }
+
 provider "aws" {
-access_key  =var.access_key
-secret_key = var.secret_key
-region = var.region
+  region     = var.region        
+  access_key = var.access_key    
+  secret_key = var.secret_key    
 }
+
 resource "aws_dynamodb_table" "resumeTable" {
   name            = "atieno_resume"
   billing_mode    = "PROVISIONED"
